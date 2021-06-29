b15123854eb2f23f0060698a4ea5e099eff7982e
Ignacio Zunino <ignaciozunino@MacBook-Pro-de-Ignacio.local>
2021-06-29 08:27:42 -0300

    change sanity studio name

---
+++ components/blog/intro.js
@@ -0,0 +1,11 @@
+import { CMS_NAME, CMS_URL } from './config/constants'
+
+export default function Intro() {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
+        Move The Chain Blog
+      </h1>
+    </section>
+  )
+}
